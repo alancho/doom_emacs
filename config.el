8fb34187d47763b29a18c9d7755f2b4071827715
Alan Severini <alancho@debian>
2020-12-09 16:09:52 -0300

    Primer commit

---
+++ config.el
@@ -0,0 +1,107 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Alan Severini"
+      user-mail-address "severini.alan@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+(set-face-attribute 'default nil :height 105 :family "monospace" :weight 'normal :width 'normal)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Dropbox/gtd/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(defun then_R_operator ()
+  "R - %>% operator or 'then' pipe operator"
+  (interactive)
+  (just-one-space 1)
+  (insert "%>%")
+  (reindent-then-newline-and-indent))
+
+(defun then_ggplot_plus ()
+  (interactive)
+  (just-one-space 1)
+  (insert "+")
+  (reindent-then-newline-and-indent))
+
+(defun forbid-vertical-split ()
+  "Only permit horizontal window splits."
+  (setq-local split-height-threshold nil)
+  (setq-local split-width-threshold 0))
+
+(use-package! ess
+  :ensure t
+  :hook (ess-mode-hook . forbid-vertical-split)
+  :init
+  (require 'ess-site)
+  (require 'ess-mode)
+  (require 'ess-r-mode)
+  (setq ess-indent-with-fancy-comments nil)
+  :config
+  (setq ess-ask-for-ess-directory nil
+	ess-local-process-name "R"
+	ansi-color-for-comint-mode 'filter
+	comint-scroll-to-bottom-on-input t
+	comint-scroll-to-bottom-on-output t
+	comint-move-point-for-output t
+	;; ess-eval-visibly-p t
+        ess-use-flymake nil
+        ess-eval-visibly 'nowait
+	ess-default-style 'RStudio
+	fill-column 72
+	comment-auto-fill-only-comments t)
+  ;; (auto-fill-mode t)
+  :bind (:map ess-mode-map
+	 ("C-<return>" . 'then_R_operator)
+	 ("M-<return>" . 'then_ggplot_plus)
+	 ("_" . 'ess-insert-assign)
+	 ("S-<return>" . 'ess-eval-region-or-function-or-paragraph-and-step)
+         :map inferior-ess-r-mode-map
+	 ("C-<return>" . 'then_R_operator)
+	 ("M-<return>" . 'then_ggplot_plus)
+	 ("_" . 'ess-insert-assign)
+         ("S-<return>" . 'ess-eval-region-or-function-or-paragraph-and-step)))
