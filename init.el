9f62c19fe3df01237f0982578361d106e43edd0b
Alan Severini <alancho@debian.INTA.AR>
2020-12-14 15:45:20 -0300

    Usemos en org solo aquello que necesitamos

--- init.el
+++ init.el
@@ -146,14 +146,10 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +journal
-            +roam
+       (org +roam
             +pretty
             +pandoc
-            +present
-            +dragndrop
-            +pomodoro
-            +jupyter)      ; organize your plain life in plain text
+            +pomodoro)      ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
