bc894110a73bffae3459c9c6e10d83fa52216666
Alan Severini <alancho@debian>
2020-12-15 13:19:13 -0300

    Noter y format on save

--- init.el
+++ init.el
@@ -55,7 +55,7 @@
        ;; (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       ;;(format +onsave)  ; automated prettiness
+       (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -150,7 +150,8 @@
             +roam
             +pretty
             +pandoc
-            +pomodoro)      ; organize your plain life in plain text
+            +pomodoro
+            +noter)      ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
