012a7e670e707707c38108839677cf5785965f86
Alan Severini <alancho@debian.INTA.AR>
2021-01-05 16:56:32 -0300

    Volvamos a darle una oportunidad a org-pomodoro

--- init.el
+++ init.el
@@ -148,7 +148,9 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +roam
-            +pretty)         ; organize your plain life in plain text
+            +pretty
+            +journal
+            +pomodoro)         ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
