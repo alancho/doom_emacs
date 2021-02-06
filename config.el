a9c990027b11b43610991af31273b49271047b64
Alan Severini <alancho@debian>
2021-02-05 22:15:57 -0300

    Necesito eval-visibly para poder hacer C-c C-c en R

--- config.el
+++ config.el
@@ -98,10 +98,10 @@
 	comint-scroll-to-bottom-on-input t
 	comint-scroll-to-bottom-on-output t
 	comint-move-point-for-output t
-	ess-eval-visibly-p nil
+	;; ess-eval-visibly-p nil
         ess-use-flymake nil
         ;; ess-eval-visibly 'nowait
-	ess-eval-visibly nil
+	ess-eval-visibly t
 	ess-default-style 'RStudio
 	fill-column 72
 	comment-auto-fill-only-comments t)
