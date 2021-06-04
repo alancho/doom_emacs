bbf506fc154130915d61889db8bf6db8461713bb
Alan Severini <alancho@debian.INTA.AR>
2021-06-04 16:02:59 -0300

    Let's eval visibly

--- config.el
+++ config.el
@@ -101,7 +101,7 @@
 	comint-scroll-to-bottom-on-output t
 	comint-move-point-for-output t
         ess-use-flymake nil
-        ess-eval-visibly nil
+        ess-eval-visibly t
 	ess-default-style 'RStudio
 	fill-column 72
 	comment-auto-fill-only-comments t)
