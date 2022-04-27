216638c528b5a916c81f85184b9b6016b2865259
Alan Severini <alancho@debian>
2022-04-27 06:43:41 -0300

    Más pavadas

--- init.el
+++ init.el
@@ -147,16 +147,11 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pretty
-            +pomodoro
-            +roam2
-            +dragndrop)         ; organize your plain life in plain text
+       (org +pretty +roam2 +dragndrop) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +conda
-               +pyright
-               +lsp)     ; beautiful is better than ugly
+       (python +conda +pyright +lsp) ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
