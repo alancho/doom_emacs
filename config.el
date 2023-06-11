b436125594eb80165696c5203636790633855edf
Alan Severini <severini.alan@gmail.com>
2023-06-11 19:54:28 -0300

    Ubuntu mono me parece mejor fuente que Fira Code

--- config.el
+++ config.el
@@ -18,15 +18,13 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Fira Code" :size 14 :weight 'semi-light)
+(setq doom-font (font-spec :family "Ubuntu Mono" :size 14 :weight 'normal)
       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-vibrant)
-
-;; (set-face-attribute 'default nil :height 105 :family "dejavu" :weight 'normal :width 'normal)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
