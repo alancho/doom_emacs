823496556cfd3b5b6a2e439f7e125c6328c0b3cf
Alan Severini <severini.alan@gmail.com>
2023-07-21 21:56:54 -0300

    Para poner setupfile en org header

--- config.el
+++ config.el
@@ -268,3 +268,17 @@
       "b" #'citar-insert-citation)
 
 (use-package! oxr)
+
+(after! ox-latex
+  (add-to-list 'org-latex-classes
+               '("org-plain-latex"
+                 "\\documentclass{article}
+           [NO-DEFAULT-PACKAGES]
+           [PACKAGES]
+           [EXTRA]"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+
