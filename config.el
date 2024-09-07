5e1f4e6d2d7dd5633dbc18b9d85302697c7e896b
alancho <severini.alan@gmail.com>
2024-09-07 18:49:06 +1000

    Para que los filenames de denote no tengan special characters

--- config.el
+++ config.el
@@ -202,6 +202,49 @@
 
 (setq citar-open-entry-function #'citar-open-entry-in-zotero)
 
+(defun denote--slug-accents (text)
+  "Return the slug of NODE."
+  (let ((slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+                           768 ; U+0300 COMBINING GRAVE ACCENT
+                           769 ; U+0301 COMBINING ACUTE ACCENT
+                           770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
+                           771 ; U+0303 COMBINING TILDE
+                           772 ; U+0304 COMBINING MACRON
+                           774 ; U+0306 COMBINING BREVE
+                           775 ; U+0307 COMBINING DOT ABOVE
+                           776 ; U+0308 COMBINING DIAERESIS
+                           777 ; U+0309 COMBINING HOOK ABOVE
+                           778 ; U+030A COMBINING RING ABOVE
+                           779 ; U+030B COMBINING DOUBLE ACUTE ACCENT
+                           780 ; U+030C COMBINING CARON
+                           795 ; U+031B COMBINING HORN
+                           803 ; U+0323 COMBINING DOT BELOW
+                           804 ; U+0324 COMBINING DIAERESIS BELOW
+                           805 ; U+0325 COMBINING RING BELOW
+                           807 ; U+0327 COMBINING CEDILLA
+                           813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+                           814 ; U+032E COMBINING BREVE BELOW
+                           816 ; U+0330 COMBINING TILDE BELOW
+                           817 ; U+0331 COMBINING MACRON BELOW
+                           )))
+    (cl-flet* ((nonspacing-mark-p (char) (memq char slug-trim-chars))
+               (strip-nonspacing-marks (s) (string-glyph-compose
+                                            (apply #'string
+                                                   (seq-remove #'nonspacing-mark-p
+                                                               (string-glyph-decompose s)))))
+               (cl-replace (text pair) (replace-regexp-in-string (car pair) (cdr pair) text)))
+      (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-") ;; convert anything not alphanumeric
+                      ("--*" . "-")                   ;; remove sequential underscores
+                      ("^-" . "")                     ;; remove starting underscore
+                      ("-$" . "")))                   ;; remove ending underscore
+             (slug (-reduce-from #'cl-replace (strip-nonspacing-marks text) pairs)))
+        (downcase slug)))))
+
+(defun my/denote-sluggify-title (str)
+  "Make STR an appropriate slug for title."
+  (denote--slug-accents (downcase (denote--slug-hyphenate (denote--slug-no-punct str)))))
+
+
 (use-package! denote
   :config
   (setq denote-directory (expand-file-name "~/Dropbox/denotes/"))
@@ -214,6 +257,10 @@
   (setq denote-date-prompt-use-org-read-date t)
   (setq denote-backlinks-show-context t)
   (setq denote-dired-directories (list denote-directory))
+  (setq denote-file-name-slug-functions
+        '((title . my/denote-sluggify-title)
+          (signature . denote-sluggify-signature)
+          (keywords . denote-sluggify-keywords)))
   :hook
   (dired-mode . denote-dired-mode-in-directories)
   :bind
