c57f640d79a11b6d231e7d707c54a31fe3bfbd59
alancho <severini.alan@gmail.com>
2025-02-05 11:34:36 +1000

    Mejor metamos enter para poder acceder a links

--- config.el
+++ config.el
@@ -123,27 +123,107 @@
 
 (setq projectile-track-known-projects-automatically nil)
 
-;; (setq company-global-modes
-;;       '(not ess-r-mode
-;;         inferior-ess-r-mode
-;;         emacs-lisp-mode
-;;         eshell-mode
-;;         markdown-mode
-;;         org-mode
-;;         shell-mode
-;;         text-mode
-;;         latex-mode))
+(setq company-global-modes
+      '(not ess-r-mode
+        inferior-ess-r-mode
+        emacs-lisp-mode
+        eshell-mode
+        markdown-mode
+        org-mode
+        shell-mode
+        text-mode
+        latex-mode))
 
 (add-hook 'markdown-mode-hook 'pandoc-mode)
+
+
+;; (defun +org-init-capture-defaults-h ()
+;;   "Sets up some reasonable defaults, as well as two `org-capture' workflows that
+;; I like:
+
+;; 1. The traditional way: invoking `org-capture' directly, via SPC X, or through
+;;    the :cap ex command.
+;; 2. Through a org-capture popup frame that is invoked from outside Emacs (the
+;;    ~/.emacs.d/bin/org-capture script). This can be invoked from qutebrowser,
+;;    vimperator, dmenu or a global keybinding."
+;;   (setq org-default-notes-file
+;;         (expand-file-name +org-capture-notes-file org-directory)
+;;         +org-capture-journal-file
+;;         (expand-file-name +org-capture-journal-file org-directory)
+;;         org-capture-templates
+;;         '(("t" "Personal todo" entry
+;;            (file+headline +org-capture-todo-file "Inbox")
+;;            "* [ ] %?\n%i\n%a" :prepend t)
+;;           ("n" "Personal notes" entry
+;;            (file+headline +org-capture-notes-file "Inbox")
+;;            "* %u %?\n%i\n%a" :prepend t)
+;;           ("j" "Journal" entry
+;;            (file denote-journal-extras-path-to-new-or-existing-entry)
+;;            "* [%(format-time-string \"%H:%M\")] %?\n%i\n%a"
+;;            :kill-buffer t
+;;            :empty-lines 1)
+;;           ;; ("j" "Journal" entry
+;;           ;;  (file+olp+datetree +org-capture-journal-file)
+;;           ;;  "* %U %?\n%i\n%a" :prepend t)
+
+;;           ;; Will use {project-root}/{todo,notes,changelog}.org, unless a
+;;           ;; {todo,notes,changelog}.org file is found in a parent directory.
+;;           ;; Uses the basename from `+org-capture-todo-file',
+;;           ;; `+org-capture-changelog-file' and `+org-capture-notes-file'.
+;;           ("p" "Templates for projects")
+;;           ("pt" "Project-local todo" entry  ; {project-root}/todo.org
+;;            (file+headline +org-capture-project-todo-file "Inbox")
+;;            "* TODO %?\n%i\n%a" :prepend t)
+;;           ("pn" "Project-local notes" entry  ; {project-root}/notes.org
+;;            (file+headline +org-capture-project-notes-file "Inbox")
+;;            "* %U %?\n%i\n%a" :prepend t)
+;;           ("pc" "Project-local changelog" entry  ; {project-root}/changelog.org
+;;            (file+headline +org-capture-project-changelog-file "Unreleased")
+;;            "* %U %?\n%i\n%a" :prepend t)
+
+;;           ;; Will use {org-directory}/{+org-capture-projects-file} and store
+;;           ;; these under {ProjectName}/{Tasks,Notes,Changelog} headings. They
+;;           ;; support `:parents' to specify what headings to put them under, e.g.
+;;           ;; :parents ("Projects")
+;;           ("o" "Centralized templates for projects")
+;;           ("ot" "Project todo" entry
+;;            (function +org-capture-central-project-todo-file)
+;;            "* TODO %?\n %i\n %a"
+;;            :heading "Tasks"
+;;            :prepend nil)
+;;           ("on" "Project notes" entry
+;;            (function +org-capture-central-project-notes-file)
+;;            "* %U %?\n %i\n %a"
+;;            :heading "Notes"
+;;            :prepend t)
+;;           ("oc" "Project changelog" entry
+;;            (function +org-capture-central-project-changelog-file)
+;;            "* %U %?\n %i\n %a"
+;;            :heading "Changelog"
+;;            :prepend t)))
+;;   )
+
+;; (use-package! org
+;;   :preface
+;;   (add-hook! 'org-load-hook
+;;              #'+org-init-capture-defaults-h)
+;;   :config
+;;   (setq org-support-shift-select 'always
+;;         org-return-follows-link nil
+;;         org-hide-emphasis-markers t
+;;         org-level-color-stars-only nil
+;;         org-replace-disputed-keys t
+;;         org-image-actual-width '(500)))
 
 (use-package! org
   :config
   (setq org-support-shift-select 'always
-        org-return-follows-link nil
+        org-return-follows-link t
         org-hide-emphasis-markers t
         org-level-color-stars-only nil
         org-replace-disputed-keys t
         org-image-actual-width '(500)))
+
 
 ;; Para evitar que el tamaño de la fuente se vea reducida con superscripts o subscripts
 (setq font-latex-fontify-script nil)
