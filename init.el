75467198fd64502857bbbba46aa16746c6a3906e
Alan Severini <severini.alan@gmail.com>
2025-10-09 12:40:54 +1000

    fix: Fix indentation in direnv configuration

--- init.el
+++ init.el
@@ -88,7 +88,7 @@
        ;;ansible
        biblio
        ;;debugger          ; FIXME stepping through code, to help you add bugs
-        (direnv +envrc)
+       (direnv +envrc)
        docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
