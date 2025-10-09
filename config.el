c711fd253dd7ea4bb93ce1f9466b3dabafb00b52
Alan Severini <severini.alan@gmail.com>
2025-10-09 13:15:38 +1000

    feat(yasnippet): Rebind expansion key from TAB to C-<tab>
    Disable default TAB key bindings and use C-<tab> for snippet expansion
    to avoid conflicts with other TAB-based completions.

--- config.el
+++ config.el
@@ -114,6 +114,12 @@
 (use-package! company-box
   :hook (company-mode . company-box-mode))
 
+;; Rebind yasnippet: disable TAB, use C-<tab> to expand
+(after! yasnippet
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "TAB") nil)
+  (define-key yas-minor-mode-map (kbd "C-<tab>") #'yas-expand))
+
 ;;; ========================================================================
 ;;; SPELL CHECKING
 ;;; ========================================================================
